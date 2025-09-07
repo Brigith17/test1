dbb22c4a8d62b08d1ef183e0be0ddc3892b70c67
Brigith Palmay <brigith.palmay@ces.ec>
2025-09-07 12:58:15 -0500

    mvp 1

--- core/Router.php
+++ core/Router.php
@@ -2,9 +2,7 @@
 class Router {
     public function run(){
         $controller = $_GET['controller']??'home';
-        $action = $_GET['action']??'index';
-        echo "Controller: ";
-        
+        $action = $_GET['action']??'index';        
         $controllerClass = ucfirst($controller).'Controller';
         require_once "../controllers/$controllerClass.php";
         $obj = new $controllerClass();
